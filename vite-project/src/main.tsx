d6499bb20adee39db259360b36eafcd569a4fdd1
Anmolcoding <Panmol607@gmail.com>
2024-08-28 21:21:06 +0530

    fix payments

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 import "./index.css";
 import ReactDOM from "react-dom/client";
-// import Header from "./Components/Header";
-// import ConfirmPay from "./Components/ConfirmPay";
-// import Details from "./Components/Details";
-// import Information from "./Components/Information";
-// import Payment from "./Components/Payment";
+import Header from "./Components/Header";
+import ConfirmPay from "./Components/ConfirmPay";
+import Details from "./Components/Details";
+import Information from "./Components/Information";
+import Payment from "./Components/Payment";
 import BookingSummary from "./Components/BookingSummary";
-// import Faq from "./Components/Faq";
-// import Footer from "./Components/Footer";
+import Faq from "./Components/Faq";
+import Footer from "./Components/Footer";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <Header />
+    <Header />
     <ConfirmPay />
     <Details />
     <Information />
-    <Payment /> */}
+    <Payment />
     <BookingSummary />
-    {/* <Faq />
-    <Footer /> */}
+    <Faq />
+    <Footer />
   </React.StrictMode>
 );
