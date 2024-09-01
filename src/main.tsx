b4be390350bf7d6f757b585bcf44185b874d1c8c
Anmolcoding <Panmol607@gmail.com>
2024-09-01 23:03:20 +0530

    change layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,28 @@
 import React from "react";
 import "./index.css";
 import ReactDOM from "react-dom/client";
-// import Header from "./Components/Header";
+import Header from "./Components/Header";
 import ConfirmPay from "./Components/ConfirmPay";
-// import Details from "./Components/Details";
-// import Information from "./Components/Information";
-// import Payment from "./Components/Payment";
-// import BookingSummary from "./Components/BookingSummary";
-// import Faq from "./Components/Faq";
-// import Footer from "./Components/Footer";
+import Details from "./Components/Details";
+import Information from "./Components/Information";
+import Payment from "./Components/Payment";
+import BookingSummary from "./Components/BookingSummary";
+import Faq from "./Components/Faq";
+import Footer from "./Components/Footer";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <Header /> */}
-    {/* <div className="main">
-      <div className="mainweb"> */}
-    <ConfirmPay />
-    {/* <Details />
+    <Header />
+    <div className="main">
+      <div className="mainweb">
+        <ConfirmPay />
+        <Details />
         <Information />
         <Payment />
       </div>
       <BookingSummary />
     </div>
     <Faq />
-    <Footer /> */}
+    <Footer />
   </React.StrictMode>
 );
